aba85e9ffe74140c7b0625b11a2c3647127f0703
HapKat <katharina.hamann@student.uni-tuebingen.de>
2025-12-08 17:00:52 +0100

    added credits

--- TitleScreen/title_screen.gd
+++ TitleScreen/title_screen.gd
@@ -2,13 +2,10 @@ extends Control
 
 # MAIN BUTTONS
 func _on_start_pressed() -> void:
-	print("Start game")
 	get_tree().change_scene_to_file("res://generalGame/scenes/main_scene.tscn")
 
 func _on_credits_pressed() -> void:
-	print("Show credits")
 	get_tree().change_scene_to_file("res://TitleScreen/credits.tscn")
 
 func _on_quit_pressed() -> void:
-	print("Quit game")
 	get_tree().quit()
