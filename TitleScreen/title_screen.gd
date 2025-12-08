0b85d3d2c32b0e67ce620a094d5d4604a0771704
HapKat <katharina.hamann@student.uni-tuebingen.de>
2025-12-08 16:50:16 +0100

    added main menu
    I added buttons for start, credits and quit to the title screen

--- TitleScreen/title_screen.gd
+++ TitleScreen/title_screen.gd
@@ -1,15 +1,9 @@
 extends Control
 
 # MAIN BUTTONS
-
 func _on_start_pressed() -> void:
 	print("Start game")
 	get_tree().change_scene_to_file("res://generalGame/scenes/main_scene.tscn")
-
-func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("continue"):
-		print("Start game")
-		get_tree().change_scene_to_file("res://generalGame/scenes/main_scene.tscn")
 
 func _on_credits_pressed() -> void:
 	print("Show credits")
