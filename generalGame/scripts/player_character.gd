d855800ef70f38cba6c787ddbce4fc7d5ae4d96a
Katharina Hamann <katharina.hamann@student.uni-tuebingen.de>
2025-12-22 16:17:21 +0100

    Created sandbox version to implement oder stuff
    All of these things are still placeholders, but they will help me to implement other stuff

--- generalGame/scripts/player_character.gd
+++ generalGame/scripts/player_character.gd
@@ -1,39 +1,17 @@
 extends CharacterBody2D
 
-@onready var anim = $AnimatedSprite2D
-
-const SPEED = 300.0
-
-var last_facing_right := true  # remembers which direction we faced last
+const SPEED := 100.0
+var talking = false
 
 func _physics_process(_delta: float) -> void:
-	var x_input := Input.get_axis("move_left", "move_right")
-	var y_input := Input.get_axis("move_up", "move_down")
+	var input_vector := Vector2(
+		Input.get_axis("walk_left", "walk_right"),
+		Input.get_axis("walk_up", "walk_down")
+	)
 
-	# Movement
-	velocity.x = x_input * SPEED
-	velocity.y = y_input * SPEED
-
-	# --- Animation Logic ---
-	if (x_input != 0 or y_input != 0):
-		# walking
-		anim.play("walking")
-
-		# Facing direction
-		if x_input > 0:
-			anim.flip_h = true
-			last_facing_right = false
-		elif x_input < 0:
-			anim.flip_h = false
-			last_facing_right = true
-		else:
-			# Moving only up/down → keep last horizontal direction
-			anim.flip_h = not last_facing_right
-	else:
-		# idle
-		anim.play("idle")
-
-		# face last direction while idle
-		anim.flip_h = not last_facing_right
-
+	velocity = input_vector.normalized() * SPEED
 	move_and_slide()
+
+
+func _on_goose_child_entered_tree(node: Node) -> void:
+	talking = true
